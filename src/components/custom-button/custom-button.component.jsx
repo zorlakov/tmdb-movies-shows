d6ce012dfa6081226631e9128845c44349fffdbe
unknown <zoka_cs_pro@hotmail.com>
2020-09-22 05:59:10 +0200

    Updated styling for navigation buttons

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import tvIcon from '../../assets/television.svg';
-import movieIcon from '../../assets/clapperboard.svg';
 import './custom-button.styles.css';
 
 const CustomButton = ({ name }) => (
-  <button className='custom-button'>
-    {name === 'Movies' ? (
-      <img alt='icon' className='icon' src={movieIcon} />
-    ) : (
-      <img alt='icon' className='icon' src={tvIcon} />
-    )}
-    {name}
-  </button>
+  <button className='custom-button'>{name}</button>
 );
 
 export default CustomButton;
